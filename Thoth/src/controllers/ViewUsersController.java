79e6f8b307a2cdab1e3d6b5088e06f820365aa85
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-03-22 16:11:09 +0100

    Poprawiono błędy

--- Thoth/src/controllers/ViewUsersController.java
+++ Thoth/src/controllers/ViewUsersController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controllers;
 
 import java.io.IOException;
@@ -35,7 +30,7 @@ public class ViewUsersController implements Initializable {
         {
             temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/add_employee.fxml"));
         }
-        
+
         Scene temporaryLoginScene = new Scene(temporaryLoginParent);
 
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
