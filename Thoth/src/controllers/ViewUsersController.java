f9a5720c61a0d8b0c69df8d73fa38b569a5df887
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-03-22 17:46:21 +0100

    Poprawiono wygląd okien, dodano zakładki dla okna przegladania pracownikow i dodania pracownika, dodano logout w menu, I19L3GR7-35 I19L3GR7-36

--- Thoth/src/controllers/ViewUsersController.java
+++ Thoth/src/controllers/ViewUsersController.java
@@ -1,46 +1,42 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 
 public class ViewUsersController implements Initializable {
 
-    public void switchscene(ActionEvent event) throws IOException {
-        System.out.println(event.getSource().toString());
+    @FXML
+    MenuItem logout;
+    MenuItem back;
 
-        Parent temporaryLoginParent = null;
-        if(event.getSource().toString().contains("logout") == true) //wylogowanie
-        {
-            temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/FXMLDocument.fxml"));
+    Stage stage;
+
+    @FXML Parent root;
+
+    public void logout(ActionEvent actionEvent) throws IOException {
+        if(actionEvent.getSource()==logout){
+            stage = (Stage) root.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("../fxmlfiles/FXMLDocument.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         }
-        if(event.getSource().toString().contains("back") == true) //powrót
-        {
-            temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/TemporaryLogIn.fxml"));
-        }
-        if(event.getSource().toString().contains("add_employee") == true) // dodawanie pracownika
-        {
-            temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/add_employee.fxml"));
-        }
 
-        Scene temporaryLoginScene = new Scene(temporaryLoginParent);
-
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(temporaryLoginScene);
-        window.show();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
 }
