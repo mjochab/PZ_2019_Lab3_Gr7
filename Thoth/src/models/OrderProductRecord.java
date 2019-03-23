ec243ef6d64cf36120f85f7c83e55a84190b31d6
Kamil Bania <kamilbania2@gmail.com>
2019-03-23 23:15:01 +0100

    Podpiecie kontrolerow pod widoki logistic orders i main view logistic, rozbudowane klas tymczasowych, test wprowadzania danych do TableView w widokach I19L3GR7-37

--- Thoth/src/models/OrderProductRecord.java
+++ Thoth/src/models/OrderProductRecord.java
@@ -1,4 +1,47 @@
 package models;
 
 public class OrderProductRecord {
+    private Product product;
+    private Integer count;
+    private Double totalPrice;
+
+    public OrderProductRecord(Product product, Integer count)
+    {
+        setProduct(product);
+        setCount(count);
+        calculateTotalPrice();
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        if(count < 0)
+            count *= -1;
+
+        this.count = count;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void calculateTotalPrice()
+    {
+        if(this.product != null)
+            this.totalPrice = this.product.getPrice() * this.count;
+        else
+            this.totalPrice = new Double(0);
+    }
+
+
 }
