79558b70adb1a09f549b127a10a4ca0f51103e39
wgalka <wgalka2@op.pl>
2019-03-23 22:20:33 +0100

    I19L3GR7-33 I19L3GR7-34 #in-progress Widoki okien analityka (ceny oraz dane sprzedażowe)

---
+++ Thoth/src/controllers/AnalystController.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnalystController implements Initializable {
+
+    public void switchscene(ActionEvent event) throws IOException { //zmiana sceny BUTTON
+        System.out.println(event.getSource().toString());
+        Parent temporaryLoginParent = null;
+        Scene temporaryLoginScene = null;
+        temporaryLoginScene = new Scene(temporaryLoginParent);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(temporaryLoginScene);
+        window.show();
+    }
+
+    @FXML
+    MenuItem logout, back;
+    Stage stage;
+
+    @FXML
+    Parent root;
+
+    public void menuitemaction(ActionEvent actionEvent) throws IOException { //cofanie i wylogowanie na MENU ITEM
+        stage = (Stage) root.getScene().getWindow();
+        if(actionEvent.getSource() == logout)
+        {
+            root = FXMLLoader.load(getClass().getResource("../fxmlfiles/FXMLDocument.fxml"));
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- Thoth/src/controllers/FXMLDocumentController.java
+++ Thoth/src/controllers/FXMLDocumentController.java
@@ -32,6 +32,10 @@ public class FXMLDocumentController implements Initializable {
         {
             temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/main_window_warehouse.fxml"));
         }
+        if(event.getSource().toString().contains("analyst") == true) //okno magazynu
+        {
+            temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/main_window_analyst.fxml"));
+        }
 
         Scene temporaryLoginScene = new Scene(temporaryLoginParent);
 
