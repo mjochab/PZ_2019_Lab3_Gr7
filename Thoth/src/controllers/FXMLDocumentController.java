a05c5192f99f8d37fef9ec01c249d785aa951f4b
kompotmalinowy <wgalka2@op.pl>
2019-03-24 19:36:42 +0100

    Merge branch 'sklep' of https://github.com/mjochab/PZ_2019_Lab3_Gr7 into admin_laczenie_zakladek
    # Conflicts:
    #	.gitignore
    #	Thoth/src/controllers/FXMLDocumentController.java
    #	Thoth/src/fxmlfiles/TemporaryLogIn.fxml

--- Thoth/src/controllers/FXMLDocumentController.java
+++ Thoth/src/controllers/FXMLDocumentController.java
@@ -4,14 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class FXMLDocumentController implements Initializable {
@@ -34,7 +31,7 @@ public class FXMLDocumentController implements Initializable {
         }
         if(event.getSource().toString().contains("employee_shop") == true) //okno sklepu
         {
-            temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/shopView.fxml"));
+            temporaryLoginParent = FXMLLoader.load(getClass().getResource("../fxmlfiles/main_window_shop.fxml"));
         }
         if(event.getSource().toString().contains("analyst") == true) //okno analityka
         {
