ac72ccd32c149b922aedbf4871b84731f8ee8173
Kamil Bania <kamilbania2@gmail.com>
2019-03-25 11:34:28 +0100

    Dodanie obslugi powrotu z widoku zamowienia zlozonego I19L3GR7-39

--- Thoth/src/controllers/ComplexOrderDetailsController.java
+++ Thoth/src/controllers/ComplexOrderDetailsController.java
@@ -1,4 +1,41 @@
 package controllers;
 
-public class ComplexOrderDetailsController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ComplexOrderDetailsController implements Initializable {
+    @FXML
+    private Button backButton;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void goBack(ActionEvent event)
+    {
+        Stage stg = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Parent par = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxmlfiles/main_view_logistic.fxml"));
+            par = loader.load();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stg.setScene(new Scene(par));
+    }
 }
