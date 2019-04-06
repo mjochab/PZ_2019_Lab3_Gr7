692676b38b7e05ea3fc5af388810dd480f3d44df
Kamil Bania <30506633+Kagroth@users.noreply.github.com>
2019-04-06 23:07:35 +0200

    Dodanie kolejnych polecen INSERT

--- Thoth/src/database/dataInserts.sql
+++ Thoth/src/database/dataInserts.sql
@@ -11,20 +11,15 @@ insert into object (zipcode, city, street, isshop) values ('36-006', 'Warszawa',
 insert into object (zipcode, city, street, isshop) values ('34-056', 'Poznań', 'Aleja Wolności', '1');
 
 
-
 insert into user (FirstName, LastName, Login, Password, RoleId, State, ObjectId) values ('Adam', 'Kowalski', 'test1', 'test1', '1', '1', '1');
-
 insert into user (FirstName, LastName, Login, Password, RoleId, State, ObjectId) values ('Bartosz', 'Nowacki', 'test2', 'test2', '2', '1', '2');
-
 insert into user (FirstName, LastName, Login, Password, RoleId, State, ObjectId) values ('Tadeusz', 'Malinowski', 'test3', 'test3', '3', '1', '3');
-
 insert into user (FirstName, LastName, Login, Password, RoleId, State, ObjectId) values ('Patryk', 'Nowakowski', 'test4', 'test4', '4', '1', '4');
-
 insert into user (FirstName, LastName, Login, Password, RoleId, State, ObjectId) values ('Artur', 'Dębski', 'test5', 'test5', '5', '5', '4');
-
 insert into user (FirstName, LastName, Login, Password, RoleId, State, ObjectId) values ('Mateusz', 'Dąbrowski', 'test6', 'test6', '5', '1', '3');
 
 
+insert into user_object (ObjectId, UserId)  values ('1','1');
 insert into user_object (ObjectId, UserId)  values ('2','1');
 insert into user_object (ObjectId, UserId)  values ('3','2');
 insert into user_object (ObjectId, UserId)  values ('4','3');
@@ -34,3 +29,24 @@ insert into user_object (ObjectId, UserId)  values ('1','5');
 
 INSERT INTO `product`(`ProductId`, `Name`, `Price`, `Discount`) VALUES (null, 'Motorola Moto Z3 Play', '1000', '0');
 INSERT INTO `product`(`ProductId`, `Name`, `Price`, `Discount`) VALUES (null, 'Huawei P9 Lite', '1100', '0');
+INSERT INTO `product`(`ProductId`, `Name`, `Price`, `Discount`) VALUES (null, 'Samsung Galaxy S10', '4500', '0');
+INSERT INTO `product`(`ProductId`, `Name`, `Price`, `Discount`) VALUES (null, 'Xiaomi MI9', '500', '0');
+INSERT INTO `product`(`ProductId`, `Name`, `Price`, `Discount`) VALUES (null, 'Sony Xperia 10', '2000', '0');
+INSERT INTO `product`(`ProductId`, `Name`, `Price`, `Discount`) VALUES (null, 'Samsung Wave 3', '150', '0');
+
+
+INSERT INTO `state_on_object`(`ProductId`, `ObjectId`, `Amount`) VALUES ('1', '1', '3');
+INSERT INTO `state_on_object`(`ProductId`, `ObjectId`, `Amount`) VALUES ('2', '1', '4');
+INSERT INTO `state_on_object`(`ProductId`, `ObjectId`, `Amount`) VALUES ('3', '2', '1');
+INSERT INTO `state_on_object`(`ProductId`, `ObjectId`, `Amount`) VALUES ('1', '2', '2');
+INSERT INTO `state_on_object`(`ProductId`, `ObjectId`, `Amount`) VALUES ('5', '2', '5');
+
+INSERT INTO `receipt`(`ReceiptId`, `ObjectId`, `TotalValue`, `UserId`, `Date`) VALUES (null, '1', '0', '1', '2019-04-06');
+INSERT INTO `receipt`(`ReceiptId`, `ObjectId`, `TotalValue`, `UserId`, `Date`) VALUES (null, '1', '0', '1', '2019-04-06');
+
+INSERT INTO `product_receipt`(`ProductId`, `ReceiptId`, `Amount`, `Price`) VALUES ('1', '1', '1', (SELECT Price FROM product WHERE ProductId = 1));
+INSERT INTO `product_receipt`(`ProductId`, `ReceiptId`, `Amount`, `Price`) VALUES ('2', '1', '1', (SELECT Price FROM product WHERE ProductId = 2));
+INSERT INTO `product_receipt`(`ProductId`, `ReceiptId`, `Amount`, `Price`) VALUES ('1', '2', '1', (SELECT Price FROM product WHERE ProductId = 1));
+
+UPDATE `receipt` SET `TotalValue`= (SELECT SUM(Price) FROM product_receipt WHERE ReceiptId = 1) WHERE ReceiptId = 1;
+UPDATE `receipt` SET `TotalValue`= (SELECT SUM(Price) FROM product_receipt WHERE ReceiptId = 2) WHERE ReceiptId = 2
