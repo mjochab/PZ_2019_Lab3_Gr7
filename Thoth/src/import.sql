7055a621cc6de88c953737baf745fb8b8f4475d6
kompotmalinowy <wgalka2@op.pl>
2019-04-07 18:11:55 +0200

    DODANIE PLIKU IMPORT.SQL

---
+++ Thoth/src/import.sql
@@ -0,0 +1,12 @@
+insert into role (RoleId,Position) values (1,'Admin');
+insert into role (RoleId,Position) values (2,'Magazynier');
+insert into role (RoleId,Position) values (3,'Analityk');
+insert into role (RoleId,Position) values (4,'Sprzedawca');
+insert into role (RoleId,Position) values (5,'Logistyk');
+
+insert into object (ObjectId, ZipCode, City, Street, IsShop) values (1,'35-311', 'Rzeszów', 'Powstańców Warszawy', 1);
+insert into object (ObjectId, ZipCode, City, Street, IsShop) values (2,'32-336', 'Kraków', 'Warszawska', 1);
+insert into object (ObjectId, ZipCode, City, Street, IsShop) values (3,'36-006', 'Warszawa', 'Krakowska', 0);
+insert into object (ObjectId, ZipCode, City, Street, IsShop) values (4,'34-056', 'Poznań', 'Aleja Wolności', 1);
+
+
