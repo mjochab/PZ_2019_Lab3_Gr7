64a81598e14792ec5683a61eae43d14f02bc006f
pavvel42 <pavvel.durda@gmail.com>
2019-04-25 19:51:33 +0200

    poprawne wyswietlenie stanu magazynu oraz nowego zamowienia w tabeli, dodany konstruktor w SalesCreatorModel

---
+++ Thoth/src/controllers/EmployeeViewController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+
+import entity.Role;
+import entity.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.hibernate.Session;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import static controllers.MainWindowController.sessionFactory;
+
+public class EmployeeViewController implements Initializable {
+
+    @FXML
+    public TableView<User> employeeTable;
+    @FXML
+    public TableColumn<User, Integer> USERID;
+    @FXML
+    public TableColumn<User, String> LOGIN;
+    @FXML
+    public TableColumn<User, String> FIRSTNAME;
+    @FXML
+    public TableColumn<User, String> LASTNAME;
+    @FXML
+    public TableColumn<User, String> PASSWORD;
+    @FXML
+    public TableColumn<User, Integer> STATE;
+    @FXML
+    public TableColumn<Role, Integer> ROLEID;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        USERID.setCellValueFactory(new PropertyValueFactory<>("userId"));
+        LOGIN.setCellValueFactory(new PropertyValueFactory<User, String>("login"));
+        FIRSTNAME.setCellValueFactory(new PropertyValueFactory<User, String>("firstName"));
+        LASTNAME.setCellValueFactory(new PropertyValueFactory<User, String>("lastName"));
+        PASSWORD.setCellValueFactory(new PropertyValueFactory<User, String>("password"));
+        STATE.setCellValueFactory(new PropertyValueFactory<User, Integer>("state"));
+        ROLEID.setCellValueFactory(new PropertyValueFactory<Role, Integer>("roleId"));
+        employeeTable.setItems(getEmployee());
+        System.out.println(getEmployee().toString());
+    }
+
+    public ObservableList<User> getEmployee() {
+        ObservableList<User> userList = FXCollections.observableArrayList();
+        Session session = sessionFactory.openSession();
+        List<User> usList = session.createQuery("from User ").list();
+        for (User us : usList) {
+            //us.getRoleId().getPosition();
+            us.getUserId();
+            userList.add(us);
+
+        }
+        session.close();
+        return userList;
+    }
+
+}
--- Thoth/src/controllers/StateWarehouseController.java
+++ Thoth/src/controllers/StateWarehouseController.java
@@ -8,7 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
@@ -16,6 +15,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import models.SalesCreatorModel;
 import org.hibernate.Session;
 
 import java.io.IOException;
@@ -29,23 +29,25 @@ import static controllers.MainWindowController.sessionFactory;
 public class StateWarehouseController implements Initializable {
 
     @FXML
-    public TableView<Product> stateWarehouse;
+    public TableView stateWarehouse,new_order;
     @FXML
-    public TableColumn<Product, Integer> PRODUCTID;
+    public TableColumn PRODUCTID;
     @FXML
-    public TableColumn<Product, String> NAME;
+    public TableColumn NAME;
     @FXML
-    public TableColumn<Product, BigDecimal> PRICE;
+    public TableColumn PRICE;
     @FXML
-    public TableColumn<State_on_shop,Integer> AMOUNT;
+    public TableColumn AMOUNT;
     @FXML
-    public TableColumn<Product, Integer> DISCOUNT;
+    public TableColumn DISCOUNT;
 
     @FXML
     MenuItem logout;
 
     Stage stage;
     @FXML Parent root;
+
+    String nazwaMagazynu = null; //nazwa magazynu do przechukiwania zawartości
 
     public void menuitemaction(ActionEvent actionEvent) throws IOException { //wylogowanie na MENU ITEM
         stage = (Stage) root.getScene().getWindow();
@@ -68,21 +70,32 @@ public class StateWarehouseController implements Initializable {
             PRICE.setCellValueFactory(new PropertyValueFactory<Product, BigDecimal>("price"));
             AMOUNT.setCellValueFactory(new PropertyValueFactory<State_on_shop, Integer>("amount"));
             DISCOUNT.setCellValueFactory(new PropertyValueFactory<Product, Integer>("discount"));
-            stateWarehouse.setItems(getProducts());
-            System.out.println(getProducts().toString());
+            stateWarehouse.setItems(getProducts(nazwaMagazynu));
+            System.out.println(getProducts(nazwaMagazynu).toString());
+        }
+        if(location.toString().contains("new_order"))
+        {
+            PRODUCTID.setCellValueFactory(new PropertyValueFactory<>("productId"));
+            NAME.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
+            PRICE.setCellValueFactory(new PropertyValueFactory<Product, BigDecimal>("price"));
+            AMOUNT.setCellValueFactory(new PropertyValueFactory<State_on_shop, Integer>("amount"));
+            //DISCOUNT.setCellValueFactory(new PropertyValueFactory<Product, Integer>("discount"));
+            new_order.setItems(getProducts(nazwaMagazynu));
+            System.out.println(getProducts(nazwaMagazynu).toString());
         }
     }
 
-    public ObservableList<Product> getProducts() {
-        ObservableList<Product> enseignantList = FXCollections.observableArrayList();
+    public ObservableList<SalesCreatorModel> getProducts(String nazwaMagazynu) {
+        ObservableList<SalesCreatorModel> productList = FXCollections.observableArrayList();
         Session session = sessionFactory.openSession();
-        List<Product> eList = session.createQuery("from Product").list();
-
-        for (Product ent : eList) {
-            enseignantList.add(ent);
+        List<SalesCreatorModel> eList = session.createQuery("SELECT new models.SalesCreatorModel(p.productId, p.name, p.price, p.discount, s.amount) FROM Product p " +
+                "INNER JOIN State_on_shop s ON p.productId = s.productId INNER JOIN Shop k ON s.shopId = k.shopId WHERE s.amount > 0 AND k.city like 'R%'"
+        ).list();
+        System.out.println(eList);
+        for (SalesCreatorModel ent : eList) {
+            productList.add(ent);
         }
         session.close();
-        return enseignantList;
+        return productList;
     }
-
 }
