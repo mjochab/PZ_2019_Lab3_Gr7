7644331a344691055dc3c4ffcda1c4d23e592762
Kamil Bania <kamilbania2@gmail.com>
2019-04-25 22:04:30 +0200

    Utworzenie modelu reprezentujacego wiersz w TableView w widoku zamowienia prostego

--- Thoth/src/models/IndentProductsView.java
+++ Thoth/src/models/IndentProductsView.java
@@ -1,4 +1,27 @@
 package models;
 
+import entity.Indent;
+import entity.Indent_product;
+
 public class IndentProductsView {
+    private Indent_product products;
+    private Indent order;
+
+    public IndentProductsView() {}
+
+    public Indent_product getProducts() {
+        return products;
+    }
+
+    public void setProducts(Indent_product products) {
+        this.products = products;
+    }
+
+    public Indent getOrder() {
+        return order;
+    }
+
+    public void setOrder(Indent order) {
+        this.order = order;
+    }
 }
