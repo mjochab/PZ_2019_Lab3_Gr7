013a4f988c599ee54e955f4999724e4e0da4c247
wgalka <wgalka2@op.pl>
2019-05-05 17:09:54 +0200

    hello world

--- PdfGeneratorLib/src/pdfgeneratorlib/PdfGenerator.java
+++ PdfGeneratorLib/src/pdfgeneratorlib/PdfGenerator.java
@@ -3,6 +3,6 @@ package pdfgeneratorlib;
 public class PdfGenerator {
 
     public static void main(String[] args) {
-        // write your code here
+       System.out.println("hello world");
     }
 }
