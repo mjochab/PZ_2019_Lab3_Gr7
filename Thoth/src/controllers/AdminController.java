3d3483e28ffaf8435cb6dc4428c6cf86865f55fc
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-05-09 20:01:10 +0200

    Dodawanie i usuwanie pracownikow

--- Thoth/src/controllers/AdminController.java
+++ Thoth/src/controllers/AdminController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import entity.Role;
+import entity.Shop;
+import entity.User;
+import entity.UserShop;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,21 +14,18 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import static controllers.MainWindowController.*;
-
-import entity.Product;
 import org.hibernate.Session;
 
 public class AdminController implements Initializable {
+
     @FXML
     MenuItem logout;
     @FXML
@@ -33,6 +34,22 @@ public class AdminController implements Initializable {
     Parent root;
 
     Stage stage;
+
+    @FXML
+    private TextField tfFirstName;
+    @FXML
+    private TextField tfLastName;
+    @FXML
+    private TextField tfLogin;
+    @FXML
+    private PasswordField tfPassword;
+    @FXML
+    private ComboBox<Role> comboRoleList;
+    @FXML
+    private ComboBox<Shop> comboShopList;
+    @FXML
+    private Button btnAddEmployee;
+
 
     public void switchscene(ActionEvent event) throws IOException { //zmiana sceny BUTTON
         System.out.println(event.getSource().toString());
@@ -44,7 +61,6 @@ public class AdminController implements Initializable {
         window.show();
     }
 
-
     public void menuitemaction(ActionEvent actionEvent) throws IOException { //cofanie i wylogowanie na MENU ITEM
         stage = (Stage) root.getScene().getWindow();
         if (actionEvent.getSource() == logout) {
@@ -55,9 +71,58 @@ public class AdminController implements Initializable {
         stage.show();
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void addEmployee() { //dodawanie użytkownika do bazy
+        User u = new User();
+        UserShop us = new UserShop();
+        u.setFirstName(tfFirstName.getText());
+        u.setLastName(tfLastName.getText());
+        u.setPassword(tfPassword.getText());
+        //combo
+    }
+
+    private ObservableList<Shop> getShops() { // wybiera listę sklepów z bazy
+        ObservableList<Shop> shops = FXCollections.observableArrayList();
+        Session session = sessionFactory.openSession();
+        Shop sp = new Shop();
+        List<Shop> shopsList = session.createQuery("from Shop").list();
+        if (shops.size() > 0) {
+            sp.setCity(shops.get(0).getCity());
+            sp.setStreet(shops.get(0).getStreet());
+            sp.setZipCode(shops.get(0).getZipCode());
+            shops.addAll(shopsList);
+            session.close();
+        }
+        return shops;
+    }
+
+    public void setComboShopList() {
+        this.comboShopList.getItems().addAll(getShops());
+    }
+
+    private ObservableList<Role> getRoles() {// wybiera listę rul z bazy
+        ObservableList<Role> roles = FXCollections.observableArrayList();
+        Session session2 = sessionFactory.openSession();
+        Role rl = new Role();
+        List<Role> rolesList = session2.createQuery("SELECT r FROM Role r", Role.class).getResultList();
+        roles.addAll(rolesList);
+        session2.close();
+
+        if (roles.size() > 0) {
+            rl.setPosition(roles.get(0).getPosition());
+            // rl.addAll(rolesList);
+            session2.close();
+        }
+        session2.close();
+        return roles;
+    }
+
+    public void setComboRoleList () {
+        // this.comboRoleList.getItems.addAll(getRoles());
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
