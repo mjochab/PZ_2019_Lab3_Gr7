4dc21c49e1355714aae50ce117a3e2063dfb0ab4
AdrianGajewski <adrian.gajewski93@gmail.com>
2019-05-10 00:31:16 +0200

    Usuwanie pracownikow

--- Thoth/src/controllers/EmployeeViewController.java
+++ Thoth/src/controllers/EmployeeViewController.java
@@ -9,9 +9,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import models.EmployeeView;
 import org.hibernate.Session;
 import javafx.beans.property.SimpleStringProperty;
@@ -36,7 +38,7 @@ public class EmployeeViewController implements Initializable {
     @FXML
     public TableColumn<EmployeeView, String> PASSWORD;
     @FXML
-    public TableColumn<EmployeeView, Integer> STATE;
+    public TableColumn<EmployeeView, String> STATE;
     @FXML
     public TableColumn<EmployeeView, String> ROLEID;
     @FXML
@@ -50,11 +52,14 @@ public class EmployeeViewController implements Initializable {
         LASTNAME.setCellValueFactory(userData -> new SimpleStringProperty(userData.getValue().getUser().getLastName()));
         LOGIN.setCellValueFactory(userData -> new SimpleStringProperty(userData.getValue().getUser().getLogin()));
         PASSWORD.setCellValueFactory(userData -> new SimpleStringProperty(userData.getValue().getUser().getPassword()));
-        STATE.setCellValueFactory(userData -> new SimpleIntegerProperty(userData.getValue().getUser().getState()).asObject());
+        STATE.setCellValueFactory(userData -> new SimpleStringProperty(String.valueOf(userData.getValue().getUser().getState())));
         ROLEID.setCellValueFactory(userData -> new SimpleStringProperty(userData.getValue().getUser().getRoleId().getPosition()));
         OBJECTID.setCellValueFactory(userData -> new SimpleStringProperty(userData.getValue().getShop().toString()));
         employeeTable.setItems(getEmployee());
+        setEditableStatus();
         System.out.println(getEmployee().toString());
+
+
     }
 
 
@@ -84,5 +89,43 @@ public class EmployeeViewController implements Initializable {
     public void reloadTableView() {
         employeeTable.getItems().clear();
         employeeTable.getItems().addAll(getEmployee());
+    }
+
+    private void setEditableStatus() {
+        STATE.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        STATE.setOnEditCommit( e -> {
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).getUser().setState(Integer.parseInt(e.getNewValue()));
+            System.out.println((e.getTableView().getSelectionModel().getSelectedItem().getUser().toString()));
+
+            Session session = sessionFactory.openSession();
+
+            session.getTransaction().begin();
+            User userToUpdate = e.getTableView().getSelectionModel().getSelectedItem().getUser();
+
+
+            try {
+                session.saveOrUpdate(userToUpdate);
+                session.getTransaction().commit();
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Powodzenie");
+                alert.setContentText("Dane użytkownika zostaly zaktualizowane");
+                alert.showAndWait();
+            }
+            catch (Exception exc) {
+                System.out.println(exc);
+                session.getTransaction().rollback();
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Niepowodzenie");
+                alert.setContentText("Niepowodzenie aktualizacji danych");
+                alert.showAndWait();
+            }
+
+            session.close();
+        });
+
+        employeeTable.setEditable(true);
     }
 }
