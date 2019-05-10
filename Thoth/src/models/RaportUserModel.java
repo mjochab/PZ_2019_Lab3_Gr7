ee2fffb37f76a53b1f54f198a4c6bf6ba033cf06
wgalka <wgalka2@op.pl>
2019-05-10 14:29:22 +0200

    poprawa modelu

--- Thoth/src/models/RaportUserModel.java
+++ Thoth/src/models/RaportUserModel.java
@@ -4,11 +4,13 @@ public class RaportUserModel {
     private String firstName;
     private String lastName;
     private RaportProductModel[] soldProducts;
+    private RaportUserModel[] users;
 
-    public RaportUserModel(String firstName, String lastName, RaportProductModel[] soldProducts) {
+    public RaportUserModel(String firstName, String lastName, RaportProductModel[] soldProducts, RaportUserModel[] users) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.soldProducts = soldProducts;
+        this.users = users;
     }
 
     public String getFirstName() {
@@ -33,5 +35,13 @@ public class RaportUserModel {
 
     public void setSoldProducts(RaportProductModel[] soldProducts) {
         this.soldProducts = soldProducts;
+    }
+
+    public RaportUserModel[] getUsers() {
+        return users;
+    }
+
+    public void setUsers(RaportUserModel[] users) {
+        this.users = users;
     }
 }
