20111a1ff66d42eb0cc78eaf07d89b959691a427
wgalka <wgalka2@op.pl>
2019-05-11 17:29:03 +0200

    generowanie pdf z obiektu

--- PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/CreatePDF.java
+++ PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/CreatePDF.java
@@ -10,36 +10,7 @@ import com.itextpdf.layout.element.Table;
 import java.io.*;
 import java.util.List;
 
-public class Main {
-
-//    public static final String DEST = "PdfGeneratorLib/results/test/test.pdf";
-
-    public static void main(String[] args) throws IOException {
-//        File file = new File(DEST);
-//        file.getParentFile().mkdirs();
-//        new Main().createPdf(DEST);
-
-//        InputStream fileIs = null;
-//        ObjectInputStream objIs = null;
-//        try {
-//            fileIs = new FileInputStream("shops.object");
-//            objIs = new ObjectInputStream(fileIs);
-//            RaportModel emp = (RaportModel) objIs.readObject();
-//            System.out.println(emp);
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if(objIs != null) objIs.close();
-//            } catch (Exception ex){
-//
-//            }
-//        }
-    }
+public class CreatePDF {
 
     public void createPdf(List<RaportModel> data, String destination) throws IOException {
 
