03e0c9dd8aefabc97f2e0876d2d7c7c8a7f1d58a
wgalka <wgalka2@op.pl>
2019-05-11 17:20:31 +0200

    generowanie pdf z obiektu

--- PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/Main.java
+++ PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/Main.java
@@ -3,10 +3,12 @@ package com.pdfgeneratorlib;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 
 import java.io.*;
+import java.util.List;
 
 public class Main {
 
@@ -17,47 +19,71 @@ public class Main {
 //        file.getParentFile().mkdirs();
 //        new Main().createPdf(DEST);
 
-        InputStream fileIs = null;
-        ObjectInputStream objIs = null;
-        try {
-            fileIs = new FileInputStream("shops.object");
-            objIs = new ObjectInputStream(fileIs);
-            RaportModel emp = (RaportModel) objIs.readObject();
-            System.out.println(emp);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(objIs != null) objIs.close();
-            } catch (Exception ex){
-
-            }
-        }
+//        InputStream fileIs = null;
+//        ObjectInputStream objIs = null;
+//        try {
+//            fileIs = new FileInputStream("shops.object");
+//            objIs = new ObjectInputStream(fileIs);
+//            RaportModel emp = (RaportModel) objIs.readObject();
+//            System.out.println(emp);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if(objIs != null) objIs.close();
+//            } catch (Exception ex){
+//
+//            }
+//        }
     }
 
-    public void createPdf(RaportModel data,String destination) throws IOException {
+    public void createPdf(List<RaportModel> data, String destination) throws IOException {
 
-
+        //tworzenie pliku w którym będzie zapisany dokument
         FileOutputStream fos = new FileOutputStream(destination);
+
+        //tworzenie obiektu zapisującego
         PdfWriter writer = new PdfWriter(fos);
 
+        //utworzenie nowego dokumentu PDF
         PdfDocument pdf = new PdfDocument(writer);
 
+        //utworzenie pustego dokumentu
         Document document = new Document(pdf);
 
-        document.add(new Paragraph("hello world"));
+        //utworzenie nagłówka dokumentu
+        document.add(new Paragraph("THOTH raport sprzedażowy."));
 
-        Table table = new Table(8);
-        for (int i = 0; i < 16; i++) {
-            table.addCell("1");
-            table.addFooterCell("2");
-            //table.addCell("2");
+        for(RaportModel shop : data){
+            Table table = new Table(1);
+            Cell shopAdress = new Cell();
+            Cell productsRaport = new Cell();
+            Cell usersRaport = new Cell();
+
+            shopAdress.add(shop.getStreet()+" "+shop.getZipCode()+" "+shop.getCity()+" Zysk:"+shop.getProfit());         // Adding content to the cell
+            table.addCell(shopAdress);      // Adding cell to the table
+
+            Table productsTable = new Table(3);
+            for(RaportProductModel product : shop.getProducts()){
+
+                productsTable.addCell(product.getName());
+                productsTable.addCell(product.getSold().toString());
+                productsTable.addCell(product.getTotal_price().toString());
+            }
+            table.addCell(productsTable);
+
+            Table userTable = new Table(2);
+            for(RaportUserModel user : shop.getUsers()){
+                userTable.addCell(user.getUserId().toString());
+                userTable.addCell(user.getTotal().toString());
+            }
+            table.addCell(userTable);
+            document.add(table);
         }
-        document.add(table);
 
         document.close();
     }
