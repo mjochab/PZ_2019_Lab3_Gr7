6adefe2de6db57912fedb3011a21b5982aa5b741
wgalka <wgalka2@op.pl>
2019-05-11 17:34:18 +0200

    przeniesienie metody create pdf

--- PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/RaportModel.java
+++ PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/RaportModel.java
@@ -1,5 +1,14 @@
 package com.pdfgeneratorlib;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
@@ -110,6 +119,53 @@ public class RaportModel implements Serializable {
                 ", products=" + products +
                 ", users=" + users +
                 '}';
+    }
+
+    public void createPdf(List<RaportModel> data, String destination) throws IOException {
+
+        //tworzenie pliku w którym będzie zapisany dokument
+        FileOutputStream fos = new FileOutputStream(destination);
+
+        //tworzenie obiektu zapisującego
+        PdfWriter writer = new PdfWriter(fos);
+
+        //utworzenie nowego dokumentu PDF
+        PdfDocument pdf = new PdfDocument(writer);
+
+        //utworzenie pustego dokumentu
+        Document document = new Document(pdf);
+
+        //utworzenie nagłówka dokumentu
+        document.add(new Paragraph("THOTH raport sprzedażowy."));
+
+        for(RaportModel shop : data){
+            Table table = new Table(1);
+            Cell shopAdress = new Cell();
+            Cell productsRaport = new Cell();
+            Cell usersRaport = new Cell();
+
+            shopAdress.add(shop.getStreet()+" "+shop.getZipCode()+" "+shop.getCity()+" Zysk:"+shop.getProfit());         // Adding content to the cell
+            table.addCell(shopAdress);      // Adding cell to the table
+
+            Table productsTable = new Table(3);
+            for(RaportProductModel product : shop.getProducts()){
+
+                productsTable.addCell(product.getName());
+                productsTable.addCell(product.getSold().toString());
+                productsTable.addCell(product.getTotal_price().toString());
+            }
+            table.addCell(productsTable);
+
+            Table userTable = new Table(2);
+            for(RaportUserModel user : shop.getUsers()){
+                userTable.addCell(user.getUserId().toString());
+                userTable.addCell(user.getTotal().toString());
+            }
+            table.addCell(userTable);
+            document.add(table);
+        }
+
+        document.close();
     }
 }
 
