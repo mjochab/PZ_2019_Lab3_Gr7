d2443793cb32697d090f60d094358e781dbc1700
wgalka <wgalka2@op.pl>
2019-05-11 15:35:31 +0200

    klasy potrzebne do tworzenia obiektu przechowującego raport oraz przykładowy obiekt

---
+++ PdfGeneratorLib/src/main/java/com/pdfgeneratorlib/RaportUserModel.java
@@ -0,0 +1,38 @@
+package com.pdfgeneratorlib;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class RaportUserModel implements Serializable {
+    private Integer userId;
+    private BigDecimal total;
+
+    public RaportUserModel(Integer userId, BigDecimal total) {
+        this.userId = userId;
+        this.total = total;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "RaportUserModel{" +
+                "userId=" + userId +
+                ", total=" + total +
+                '}';
+    }
+}
