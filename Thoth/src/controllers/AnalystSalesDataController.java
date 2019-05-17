7f6950ef1393698b87e139553124166b1ebf6e77
wgalka <wgalka2@op.pl>
2019-05-17 15:34:18 +0200

    przeszukiwanie raportów po dacie

--- Thoth/src/controllers/AnalystSalesDataController.java
+++ Thoth/src/controllers/AnalystSalesDataController.java
@@ -24,6 +24,9 @@ import java.awt.*;
 import java.io.*;
 import java.math.BigDecimal;
 import java.net.URL;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -46,7 +49,8 @@ public class AnalystSalesDataController implements Initializable {
     public TableColumn STREET;
     @FXML
     public TableColumn PROFIT;
-
+    @FXML
+    public DatePicker START_DATE, END_DATE;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -62,7 +66,22 @@ public class AnalystSalesDataController implements Initializable {
     public ObservableList<SalesDataModel> getProducts() {
         ObservableList<SalesDataModel> productList = FXCollections.observableArrayList();
         Session session = sessionFactory.openSession();
-        List<SalesDataModel> eList = session.createQuery("SELECT new models.SalesDataModel(s.zipCode, s.city, s.street, SUM(r.totalValue)) FROM Shop s INNER JOIN Receipt r on s.shopId = r.shopId GROUP BY s.shopId").list();
+        List<SalesDataModel> eList = null;
+        if (START_DATE.getValue() == null && END_DATE.getValue() == null) {
+            eList = session.createQuery("SELECT new models.SalesDataModel(" +
+                    "s.zipCode, s.city, s.street, SUM(r.totalValue)) FROM Shop s INNER JOIN Receipt r on s.shopId = r.shopId " +
+                    "GROUP BY s.shopId")
+                    .list();
+        } else {
+            eList = session.createQuery("SELECT new models.SalesDataModel(" +
+                    "s.zipCode, s.city, s.street, SUM(r.totalValue)) FROM Shop s INNER JOIN Receipt r on s.shopId = r.shopId " +
+                    "Where date Between :startDate AND :endDate GROUP BY s.shopId")
+                    .setParameter("startDate", Date.from(START_DATE.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                    .setParameter("endDate", Date.from(END_DATE.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                    .list();
+            START_DATE.setValue(null);
+            END_DATE.setValue(null);
+        }
         System.out.println(eList);
         for (SalesDataModel ent : eList) {
             productList.add(ent);
@@ -82,7 +101,7 @@ public class AnalystSalesDataController implements Initializable {
 
         String path = null;
 
-        if(file != null){
+        if (file != null) {
             path = file.getAbsolutePath();
         }
 
@@ -91,7 +110,7 @@ public class AnalystSalesDataController implements Initializable {
         List<RaportModel> shops = session.createQuery("SELECT new com.pdfgeneratorlib.RaportModel(s.shopId, s.street, s.zipCode, " +
                 "s.city, SUM(r.totalValue)) from Shop s Left JOIN Receipt r ON s.shopId = r.shopId GROUP BY s.shopId").list();
 
-        for (RaportModel shop : shops){
+        for (RaportModel shop : shops) {
             Integer shopidentifier = shop.getShopId();
             List<RaportProductModel> products = session.createQuery("select new com.pdfgeneratorlib.RaportProductModel(" +
                     " s.shopId, p.name, SUM(pr.amount) as ilosc_sprzedanych, SUM(pr.price) as cena_produktow)  " +
@@ -99,7 +118,7 @@ public class AnalystSalesDataController implements Initializable {
                     "INNER JOIN Product_receipt pr ON r.receiptId = pr.receiptId " +
                     "INNER JOIN Product p ON pr.productId = p.productId " +
                     "INNER JOIN Shop s ON r.shopId = s.shopId " +
-                    "where s.shopId = :shopidentifier GROUP BY pr.productId ").setParameter("shopidentifier",shopidentifier).list();
+                    "where s.shopId = :shopidentifier GROUP BY pr.productId ").setParameter("shopidentifier", shopidentifier).list();
             shop.setProducts(products);
 
             List<RaportUserModel> users = session.createQuery("select new com.pdfgeneratorlib.RaportUserModel(u.userId, SUM(r.totalValue))" +
@@ -107,7 +126,7 @@ public class AnalystSalesDataController implements Initializable {
                     "INNER JOIN UserShop us ON u.userId = us.userId " +
                     "INNER JOIN Shop s ON us.shopId = s.shopId " +
                     "LEFT JOIN Receipt r ON u.userId = r.userId " +
-                    "WHERE u.roleId = 4 AND s.shopId = :shopidentifier GROUP BY u.userId").setParameter("shopidentifier",shopidentifier).list();
+                    "WHERE u.roleId = 4 AND s.shopId = :shopidentifier GROUP BY u.userId").setParameter("shopidentifier", shopidentifier).list();
 
             shop.setUsers(users);
         }
@@ -116,7 +135,7 @@ public class AnalystSalesDataController implements Initializable {
         System.out.println(shops.size());
         System.out.println(shops.toString());
 
-        CreatePDF.createPdf(shops,path);
+        CreatePDF.createPdf(shops, path);
 
         session.close();
 
@@ -125,9 +144,14 @@ public class AnalystSalesDataController implements Initializable {
         alert.setHeaderText("Utworzono w lokalizacji:");
         alert.setContentText(path);
         ButtonType confirm = new ButtonType("Ok", ButtonBar.ButtonData.APPLY);
-      
+
         alert.getButtonTypes().setAll(confirm);
 
         alert.showAndWait();
+    }
+
+    public void showRaportForDate() {
+        System.out.println("startdate:" + START_DATE.getValue() + "enddate:" + END_DATE.getValue());
+        salesDataTable.setItems(getProducts());
     }
 }
