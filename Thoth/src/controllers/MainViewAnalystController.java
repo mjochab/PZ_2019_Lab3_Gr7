cfd221b7284307ed661ddb8693b886c84cbf1e55
Kamil Bania <kamilbania2@gmail.com>
2019-05-18 14:25:39 +0200

    Usuniecie nieuzywanych importow

--- Thoth/src/controllers/MainViewAnalystController.java
+++ Thoth/src/controllers/MainViewAnalystController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
@@ -12,7 +11,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 import static controllers.MainWindowController.sessionContext;
