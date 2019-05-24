5a5ee224321d31b97b019b05e355ad555ba68ea5
pavvel42 <pavvel.durda@gmail.com>
2019-05-24 09:59:35 +0200

    wprowadzanie ceny w formacie BigDecimal

--- Thoth/src/controllers/WarehouseNewProductController.java
+++ Thoth/src/controllers/WarehouseNewProductController.java
@@ -43,28 +43,30 @@ public class WarehouseNewProductController implements Initializable {
     }
 
     public void addInsert(ActionEvent event) throws IOException {
-        if(isNumeric(AMOUNT.getText()) && isNumeric(PRICE.getText())){ //wprowadzono liczby
-            if(getNameProduct(tab[0]).size() == 0){ //brak takiego produktu, dodać do bazy
+        if (isNumeric(AMOUNT.getText()) && isBigDeciaml(PRICE.getText())) { //wprowadzono liczby
+            if (getNameProduct(tab[0]).size() == 0) { //brak takiego produktu, dodać do bazy
                 System.out.println("można dodać do bazy");
                 insertToDataBase();
-                NAME.setText(""); PRICE.setText(""); AMOUNT.setText("");
+                NAME.setText("");
+                PRICE.setText("");
+                AMOUNT.setText("");
                 showSuccesAllert();
             } else { //produkt jest już w bazie
-                System.out.println("Jest w bazie "+getNameProduct(tab[0]).get(0).getProductId());
-                newAlertCustom("Niepowodzenie","Produkt jest już w bazie");
+                System.out.println("Jest w bazie " + getNameProduct(tab[0]).get(0).getProductId());
+                newAlertCustom("Niepowodzenie", "Produkt jest już w bazie");
             }
         } else {
-            newAlertCustom("Niepowodzenie","Wprowadzono złe dane");
+            newAlertCustom("Niepowodzenie", "Wprowadzono złe dane");
             System.out.println("Wprowadź poprawne dane");
         }
     }
 
-    public ObservableList<ObservablePriceModel> getNameProduct(String qry){
+    public ObservableList<ObservablePriceModel> getNameProduct(String qry) {
         ObservableList<ObservablePriceModel> productList = FXCollections.observableArrayList();
         Session session = sessionFactory.openSession();
-        List<ObservablePriceModel> eList = session.createQuery(qry+":nameDevice "
-        ).setParameter("nameDevice",NAME.getText()).list();
-        System.out.println("getNameProduct "+eList);
+        List<ObservablePriceModel> eList = session.createQuery(qry + ":nameDevice "
+        ).setParameter("nameDevice", NAME.getText()).list();
+        System.out.println("getNameProduct " + eList);
         for (ObservablePriceModel ent : eList) {
             productList.add(ent);
         }
@@ -72,11 +74,11 @@ public class WarehouseNewProductController implements Initializable {
         return productList;
     }
 
-    public ObservableList<Shop> getObjectShop(){
+    public ObservableList<Shop> getObjectShop() {
         ObservableList<Shop> productList = FXCollections.observableArrayList();
         Session session = sessionFactory.openSession();
         List<Shop> eList = session.createQuery("from Shop where shopId = :pid").setParameter("pid", sessionContext.getCurrentLoggedShop().getShopId()).list();
-        System.out.println("getObjectShop "+eList);
+        System.out.println("getObjectShop " + eList);
         for (Shop ent : eList) {
             productList.add(ent);
         }
@@ -84,16 +86,15 @@ public class WarehouseNewProductController implements Initializable {
         return productList;
     }
 
-    public void insertToDataBase()
-    {
+    public void insertToDataBase() {
         Session session = sessionFactory.openSession();
-        BigDecimal wojtekDziekiZaSuperTypy = new BigDecimal(PRICE.getText());
-        Product productOB =  new Product(NAME.getText(),wojtekDziekiZaSuperTypy ,0);
+        BigDecimal big = new BigDecimal(PRICE.getText());
+        Product productOB = new Product(NAME.getText(), big, 0);
         session.save(productOB);
         System.out.println("Dodano produkt");
         session.close();
         session = sessionFactory.openSession();
-        State_on_shop product = new State_on_shop(productOB,getObjectShop().get(0),Integer.parseInt(AMOUNT.getText()));
+        State_on_shop product = new State_on_shop(productOB, getObjectShop().get(0), Integer.parseInt(AMOUNT.getText()));
         session.save(product);
         session.close();
         System.out.println("Dodano ID sklepu do produktu");
@@ -102,12 +103,21 @@ public class WarehouseNewProductController implements Initializable {
         AMOUNT.setText("");
     }
 
+    public boolean isBigDeciaml(String str) {
+        try{
+            BigDecimal big = new BigDecimal(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     // brak isDigit/isNumeric
     public static boolean isNumeric(String str) {
         try {
             Integer.parseInt(str);
             return true;
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
